fb41b7a1bd77273c00b79e78e2fb7420014d90b0
Kainy Ryu <kainy2109@gmail.com>
2020-08-25 21:11:19 +0900

    fixed the typo

--- src/components/Words.js
+++ src/components/Words.js
@@ -5,25 +5,29 @@ import Typography from '@material-ui/core/Typography';
 
 const databaseURL = "https://wordcloud-359a4.firebaseio.com";
 
-class Words extends React. Component {
+class Words extends React.Component {
     constructor() {
         super();
         this.state = {
             words: {}
         };
     }
+    
     _get() {
-        fetch(`${databaseURL}/words.json`).then(res => {
+        fetch(`${databaseURL}/words.json`)
+        .then(res => {
             if(res.status != 200) {
                 throw new Error(res.statusText);
             }
-            return res.json();
-        }).then(words => this.setState({ words: words }));
+            return res.json()
+        })
+        // .then((words) => console.log(words));
+        .then(words => this.setState({ words: words }));
     }
     shouldComponentUpdate(nextProps, nextState) {
         return nextState.words != this.state.words;
     }
-    componenetDidMount() {
+    componentDidMount() {
         this._get();
     }
     render() {
@@ -32,17 +36,20 @@ class Words extends React. Component {
                 {Object.keys(this.state.words).map(id => {
                     const word = this.state.words[id];
                     return (
-                        <Card key={id}>
-                            <CardContent>
-                                <Typography color="textSecondary" gutterBottom>
-                                    weight: {word.weight}
-                                </Typography>
-                                <Typography variant="h5" componenet="h2">
-                                    {word.word}
-                                </Typography>
-                            </CardContent>
-                        </Card>
-                    )
+                        <div key={id}>
+                            <Card>
+                                <CardContent>
+                                    <Typography color="textSecondary" gutterBottom>
+                                        Weight: {word.weight}
+                                    </Typography>
+                                    <Typography variant="h5" component="h2">
+                                        {word.word}
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+                            <br/>
+                        </div>
+                    );
                 })}
             </div>
         )
