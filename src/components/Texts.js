9d815bbd26e375b4360bb1ff5f67f44072407252
Kainy Ryu <kainy2109@gmail.com>
2020-08-29 23:18:40 +0900

    Editing texts component(not working

--- src/components/Texts.js
+++ src/components/Texts.js
@@ -1,9 +1,143 @@
+import TextTruncate from 'react-text-truncate';
 import React from 'react';
-import Card from '@material-ui/core/Card'
-import CardContent from '@material-ui/core/CardContent'
+import Card from "@material-ui/core/Card";
+import CardContent from '@material-ui/core/CardContent';
+import Typography from "@material-ui/core/Typography";
+import { withStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import Fab from "@material-ui/core/Fab";
+import AddIcon from "@material-ui/icons/Add";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import TextField from "@material-ui/core/TextField";
+
+const styles = theme => ({
+    hidden: {
+        display: 'none'
+    },
+    fab: {
+        position: 'fixed',
+        bottom: '20px',
+        right: '20px'
+    }
+})
+
+const databaseURL = "https://wordcloud-359a4.firebaseio.com/";
 
 class Texts extends React. Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            fileName: '',
+            fileContent: null,
+            text: {},
+            textName: '',
+            dialog: false
+        }
+    }
+
+
+    _get() {
+        fetch(`${databaseURL}/texts.json`)
+            .then((res) => {
+                if (res.status != 200) {
+                    throw new Error(res.statusText);
+                }
+                return res.json();
+            })
+            .then((texts) => this.setState({ texts: (texts == null) ? {} : texts }));
+    }
+
+    _post(text) {
+        return fetch(`${databaseURL}/texts.json`, {
+            method: "POST",
+            body: JSON.stringify(text),
+        })
+        .then((res) => {
+            if (res.status != 200) {
+                throw new Error(res.statusText);
+            }
+            return res.json();
+        })
+        .then((data) => {
+            let nextState = this.state.texts;
+            nextState[data.name] = text;
+            this.setState({ texts: nextState });
+        });
+    }
+
+    _delete(id) {
+        return fetch(`${databaseURL}/texts/${id}.json`, {
+            method: "DELETE",
+            })
+            .then((res) => {
+                if (res.status != 200) {
+                    throw new Error(res.statusText);
+                }
+                return res.json();
+            })
+            .then(() => {
+                let nextState = this.state.texts;
+                delete nextState[id];
+                this.setState({ texts: nextState });
+            });
+    }
+
+
+    componentDidMout() {
+        this._get();
+    }
+
+    handleDialogToggle = () =>
+        this.setState({
+            dialog: !this.state.dialog,
+            fileName: '',
+            fileContent: '',
+            textName: ''
+        });
+
+    handleValueChange = (e) => {
+        let nextState = {};
+        nextState[e.target.name] = e.target.value;
+        this.setState(nextState);
+    };
+
+    handleSubmit = () => {
+        const text = {
+            textName: this.state.textName,
+            textContent: this.state.textContent,
+        };
+        this.handleDialogToggle();
+        if (!text.textName && !text.textContent) {
+            return;
+        }
+        this._post(text);
+    };
+
+    handleDelete = (id) => {
+        this._delete(id);
+    };
+
+    handleFileChange = (e) => {
+        let reader = new FileReader();
+        reader.onload = () => {
+            let text = reader.result;
+            this.setState({
+                fileContet: text
+            });
+        }   
+        reader.readAsText(e.target.files[0], "EUC-KR");
+        this.setState({
+            fileName: e.target.value
+        })
+    }
+
     render() {
+        const { classes} = this.props;
         return (
             <Card>
                 <CardContent>
@@ -14,4 +148,4 @@ class Texts extends React. Component {
     }
 }
 
-export default Texts;
+export default withStyles(styles)(Texts);
